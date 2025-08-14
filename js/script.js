982ddc45dcd1b2984a0d8a353e87c04a2fdb3a82
Mahdi <mahdi1nap@gmail.com>
2025-08-14 14:14:33 +0330

    add js code

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
 const menuOpenButton=document.querySelector("#menu-open-button")
 const menuCloseButton=document.querySelector("#menu-close-button")
+const body=document.body
+const menu=document.querySelector(".navbar")
 
 menuOpenButton.addEventListener("click",()=>{
-    document.body.classList.toggle("show-mobile-menu")
+  document.body.classList.toggle("show-mobile-menu")
 })
 menuCloseButton.addEventListener("click",()=>{
-    menuOpenButton.click()
+  menuOpenButton.click()
 })
+
+document.addEventListener("click",(e)=>{
+  if(body.classList.contains("show-mobile-menu")){
+    if(!menu.contains(e.target)){
+      body.classList.remove("show-mobile-menu")
+    }
+    
+  }
+  
+})
+  
+
 
 
 
@@ -28,13 +42,34 @@ const swiper = new Swiper('.slider-wrapper', {
   },
   breakpoints:{
     0:{
-        slidesPerView:1
+      slidesPerView:1
     },
-     768:{
-        slidesPerView:2
+    768:{
+      slidesPerView:2
     },
-     1024:{
-        slidesPerView:3
+    1024:{
+      slidesPerView:3
     },
   }
 });
+
+
+const modal = document.querySelector(".image-modal")
+const modalImg = document.querySelectorAll(".gallery-image");
+const closeBtn = document.querySelector(".close");
+const image=document.querySelector(".modal-image")
+
+modalImg.forEach(img => {
+  img.addEventListener("click", () => {
+    modal.style.display = "flex";
+    image.src = img.src;
+  });
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+modal.addEventListener("click",()=>{
+  modal.style.display="none";
+})
